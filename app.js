4873b45db4bcd48a0d7114aed0f227130d381c93
Jonurroz <aitorurroz@gmail.com>
2025-04-02 11:21:57 +0000

    test

---
+++ app.js
@@ -0,0 +1,35 @@
+const sum = (a,b) => {
+    return a + b
+}
+
+console.log(sum(7,3))
+
+module.exports = { sum };
+
+// Declaramos una función con el nombre exacto "formEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convertimos el valor a dólares
+    let valueInDollar = valueInEuro * 1.07;
+    // Retornamos el valor en dólares
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 156.5;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.87;
+    return valueInPound;
+}
+
+// Tenemos que incluir la función en el exports para que sea exportada a otros archivos como test.js
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound }
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
+};
